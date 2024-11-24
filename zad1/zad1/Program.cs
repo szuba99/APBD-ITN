973f546aeba9480d5d36ea0d91a9ce2d79f64631
s20707 <s20707@pjwstk.edu.pl>
2024-11-24 18:05:47 +0100

    Dodano metodę do obliczania średniej z tablicy

--- zad1/zad1/Program.cs
+++ zad1/zad1/Program.cs
@@ -1,6 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("First modification");
-Console.WriteLine("second modification");
-Console.WriteLine("Third modification");
+class Program
+{
+    static double CalculateAverage(int[] numbers)
+    {
+        if (numbers.Length == 0) 
+        {
+            throw new ArgumentException("Tablica nie może być pusta.");
+        }
+
+        double sum = 0;
+        foreach (var number in numbers)
+        {
+            sum += number;
+        }
+
+        return sum / numbers.Length;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        
+        double average = CalculateAverage(numbers);
+        Console.WriteLine($"Średnia: {average}");
+        
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("First modification");
+        Console.WriteLine("second modification");
+        Console.WriteLine("Third modification");
+    }
+}
